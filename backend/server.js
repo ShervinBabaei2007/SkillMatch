a00788d52762dbb77304e8a8b5ac46f64c791a04
Shervin Babaei <shervinb22@gmail.com>
2026-05-22 12:26:37 -0700

    add console.log - future refereneces

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,11 @@ app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
+console.log("Auth:", typeof authRoutes);
+console.log("Profile:", typeof profileRoute);
+console.log("Register:", typeof registerCourseRoute);
+console.log("Workshops:", typeof workshopRoutes);
+
 app.use("/api/auth", authRoutes);
 app.use("/api/profile", profileRoute);
 app.use("/api/course", registerCourseRoute);
